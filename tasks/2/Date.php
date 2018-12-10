91ebfe78a4296c28f5e72859abccd50bc1f92dab
denv <ryabykh.denis@pdffiller.team>
2018-12-10 14:49:03 +0200

    task 2.3

--- tasks/2/Date.php
+++ tasks/2/Date.php
@@ -1,8 +1,2 @@
 <?php
 echo date("l, d F, Y");
-/**
- * Created by IntelliJ IDEA.
- * User: denv
- * Date: 12/10/18
- * Time: 1:49 PM
- */
--- tasks/2/EnvironmentVariables.php
+++ tasks/2/EnvironmentVariables.php
@@ -1,8 +1,2 @@
 <?php
 var_export($_SERVER);
-/**
- * Created by IntelliJ IDEA.
- * User: denv
- * Date: 12/10/18
- * Time: 1:58 PM
- */
